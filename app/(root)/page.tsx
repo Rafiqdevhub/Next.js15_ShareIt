bf595997b8fa76387be6331f8d9da8c04d8e4380
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-03 14:58:09 +0500

    refactor Dashboard component to improve file list rendering and structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -63,29 +63,30 @@ const Dashboard = async () => {
         {files.documents.length > 0 ? (
           <ul className="mt-5 flex flex-col gap-5">
             {files.documents.map((file: Models.Document) => (
-              <Link
-                href={file.url}
-                target="_blank"
-                className="flex items-center gap-3"
-                key={file.$id}
-              >
-                <Thumbnail
-                  type={file.type}
-                  extension={file.extension}
-                  url={file.url}
-                />
+              <li key={file.$id}>
+                <Link
+                  href={file.url}
+                  target="_blank"
+                  className="flex items-center gap-3"
+                >
+                  <Thumbnail
+                    type={file.type}
+                    extension={file.extension}
+                    url={file.url}
+                  />
 
-                <div className="recent-file-details">
-                  <div className="flex flex-col gap-1">
-                    <p className="recent-file-name">{file.name}</p>
-                    <FormattedDateTime
-                      date={file.$createdAt}
-                      className="caption"
-                    />
+                  <div className="recent-file-details">
+                    <div className="flex flex-col gap-1">
+                      <p className="recent-file-name">{file.name}</p>
+                      <FormattedDateTime
+                        date={file.$createdAt}
+                        className="caption"
+                      />
+                    </div>
+                    <ActionDropdown file={file} />
                   </div>
-                  <ActionDropdown file={file} />
-                </div>
-              </Link>
+                </Link>
+              </li>
             ))}
           </ul>
         ) : (
